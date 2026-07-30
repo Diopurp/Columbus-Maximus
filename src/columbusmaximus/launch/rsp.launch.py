85db8fd78f747601593f297f8671542436b937a4
TejoshnandaChilakalapudi <tejoshnanda.chilakalapudi@gmail.com>
2026-07-31 00:18:12 +0530

    Gazebo rendering and teleop

--- src/columbusmaximus/launch/rsp.launch.py
+++ src/columbusmaximus/launch/rsp.launch.py
@@ -7,15 +7,15 @@ from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
-    # Check if we're told to use sim time
+
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
+
     pkg_path = os.path.join(get_package_share_directory('columbusmaximus'))
     xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
 
-    # Create a robot_state_publisher node
+
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -24,7 +24,7 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    # Launch!
+
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
--- src/columbusmaximus/setup.py
+++ src/columbusmaximus/setup.py
@@ -9,18 +9,32 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'description'), glob('description/*.xacro') + glob('description/*.urdf')),
-        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.stl')),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
-    ],
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+
+    ('share/' + package_name, [
+        'package.xml',
+        'model.config',
+        'model.sdf'
+    ]),
+
+    (os.path.join('share', package_name, 'description'),
+        glob('description/*.xacro') + glob('description/*.urdf')),
+
+    (os.path.join('share', package_name, 'meshes'),
+        glob('meshes/*.stl')),
+
+    (os.path.join('share', package_name, 'launch'),
+        glob('launch/*.launch.py')),
+
+    (os.path.join('share', package_name, 'rviz'),
+        glob('rviz/*.rviz')),
+],
+
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='you@example.com',
+    maintainer='tejoshnanda_chilakalapudi',
+    maintainer_email='tejoshnanda.chilakalapudi@gmail.com',
     description='Columbus Maximus robot description',
     license='Apache-2.0',
     entry_points={
